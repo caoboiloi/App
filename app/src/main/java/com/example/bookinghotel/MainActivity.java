ae19b85272943b688932b1e9fa77f834bc976483
Tấn Lực Võ <57494001+TanLucVo@users.noreply.github.com>
2020-10-15 17:20:26 +0700

    update

--- app/src/main/java/com/example/bookinghotel/MainActivity.java
+++ app/src/main/java/com/example/bookinghotel/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bookinghotel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
@@ -13,9 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        myRef.setValue("Hello, World!");
+        Thread welcomeThread = new Thread() {
 
-        myRef.setValue("Hello, World!");
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(3000);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(MainActivity.this,
+                            login.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
     }
 }
