ec9e7aac64b7b487af65c9824047c8ae9b2aa3e4
Tấn Lực Võ <57494001+TanLucVo@users.noreply.github.com>
2020-10-19 18:49:12 +0700

    update

--- app/src/main/java/com/example/bookinghotel/LoginFragment.java
+++ app/src/main/java/com/example/bookinghotel/LoginFragment.java
@@ -1,10 +1,16 @@
 package com.example.bookinghotel;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +18,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,8 +41,9 @@ public class LoginFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     Button btn;
-    TextInputLayout emailLabel;
+    TextInputLayout emailLabel, passLabel;
     TextInputEditText etEmail,etPass;
+    private FirebaseAuth mAuth;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -70,6 +86,7 @@ public class LoginFragment extends Fragment {
         View rootView =  inflater.inflate(R.layout.fragment_login, container, false);
         btn = (Button) rootView.findViewById(R.id.btnLogin);
         emailLabel = rootView.findViewById(R.id.textFieldEmail);
+        passLabel = rootView.findViewById(R.id.passLabel);
         etEmail = rootView.findViewById(R.id.etEmail);
         etPass = rootView.findViewById(R.id.etPass);
         return rootView;
@@ -78,15 +95,87 @@ public class LoginFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        etEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!editable.toString().equals("")){
+                    emailLabel.setError("");
+                    passLabel.setHelperText("");
+                }
+            }
+        });
+        etPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(!editable.toString().equals("")){
+                    passLabel.setError("");
+                    passLabel.setHelperText("");
+                }
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email = etEmail.getText().toString();
-                String pass = etPass.getText().toString();
-                if(email.equals("") || pass.equals("")){
+                final String pass = etPass.getText().toString();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                if(email.equals("") ){
                     emailLabel.setError("Email không được để trống");
+                    emailLabel.requestFocus();
+                }else if (pass.equals("")){
+                    passLabel.setError("Mật khẩu không được để trống");
+                    passLabel.requestFocus();
+                }else if(!email.trim().matches(emailPattern)){
+                    emailLabel.setError("Email không hợp lệ");
+                }else{
+                    final ProgressDialog progress = new ProgressDialog(getActivity());
+                    progress.setMessage("Đang đăng nhập...");
+                    progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+                    progress.show();
+
+                    mAuth = FirebaseAuth.getInstance();
+                    mAuth.signInWithEmailAndPassword(email, pass)
+                            .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                    if (!task.isSuccessful()) {
+                                        // there was an error
+                                        passLabel.setHelperText("Sai tài khoản hoặc mật khẩu");
+                                        progress.dismiss();
+                                    } else {
+                                        Intent intent = new Intent(getActivity(), Home.class);
+                                        startActivity(intent);
+                                        progress.dismiss();
+                                        getActivity().finish();
+                                    }
+                                }
+                            });
                 }
             }
         });
+
     }
+
+
 }
