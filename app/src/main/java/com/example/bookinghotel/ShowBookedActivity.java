35c63c1d335231665beb807b9c0a5abff1f4b4d4
HuynhLoi <caoboiloi@users.noreply.github.com>
2020-11-29 23:53:18 +0700

    a

--- app/src/main/java/com/example/bookinghotel/ShowBookedActivity.java
+++ app/src/main/java/com/example/bookinghotel/ShowBookedActivity.java
@@ -3,12 +3,14 @@ package com.example.bookinghotel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 
 public class ShowBookedActivity extends AppCompatActivity {
 
     MaterialToolbar topAppBar;
+//    TextView name_hotel_booked, address_booked_hotel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
